a44602f7521c30f0ce6a3d430b338b78c1985548
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-11 23:24:19 -0300

    chore: adding setup project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as express from "express"
+import { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello")
+})
+
+app.listen(3000, () => console.log("listening on port 3000!"))
