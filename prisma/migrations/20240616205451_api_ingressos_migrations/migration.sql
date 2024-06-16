45a7358c6b2a4f08ef97b813a58bf486ab1cdaf6
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-16 18:03:57 -0300

    feat: generating prisma migrations

---
+++ prisma/migrations/20240616205451_api_ingressos_migrations/migration.sql
@@ -0,0 +1,82 @@
+-- CreateEnum
+CREATE TYPE "TicketType" AS ENUM ('PISTA', 'VIP');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "dtCreation" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "event" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "dtStart" TIMESTAMP(3) NOT NULL,
+    "dtEnd" TIMESTAMP(3) NOT NULL,
+    "location" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ticket" (
+    "id" TEXT NOT NULL,
+    "eventId" TEXT NOT NULL,
+    "ownerId" TEXT,
+    "type" "TicketType" NOT NULL,
+    "price" DECIMAL(18,2) NOT NULL,
+    "qtTicket" INTEGER NOT NULL,
+    "dtAvailability" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ticket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sale" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "ticketId" TEXT NOT NULL,
+    "amountTotal" DOUBLE PRECISION NOT NULL,
+    "dateSale" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "confirmationCode" TEXT NOT NULL,
+
+    CONSTRAINT "sale_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "controllerTicket" (
+    "id" TEXT NOT NULL,
+    "qtTicket" INTEGER NOT NULL,
+    "type" "TicketType" NOT NULL,
+    "price" DECIMAL(18,2) NOT NULL,
+    "eventId" TEXT NOT NULL,
+
+    CONSTRAINT "controllerTicket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sale_confirmationCode_key" ON "sale"("confirmationCode");
+
+-- AddForeignKey
+ALTER TABLE "event" ADD CONSTRAINT "event_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ticket" ADD CONSTRAINT "ticket_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sale" ADD CONSTRAINT "sale_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "sale" ADD CONSTRAINT "sale_ticketId_fkey" FOREIGN KEY ("ticketId") REFERENCES "ticket"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "controllerTicket" ADD CONSTRAINT "controllerTicket_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
