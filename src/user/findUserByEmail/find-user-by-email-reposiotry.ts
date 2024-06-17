ba92ee5dad8839de089e77fcaf438347d9aa0e75
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-16 21:16:43 -0300

    feat: add FindUserByEmailRepository

---
+++ src/user/findUserByEmail/find-user-by-email-reposiotry.ts
@@ -0,0 +1,11 @@
+import { prisma } from "@/prisma/PrismaClient/prisma"
+
+export class FindUserByEmailRepository {
+  async execute(email: string) {
+    return await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    })
+  }
+}
