77fb7594d957bb282161597e9277323c8fde115c
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-17 22:15:48 -0300

    feat: adding zod for error validations on createdUserController

---
+++ src/schemas/user/user.ts
@@ -0,0 +1,31 @@
+import { z } from "zod"
+
+export const createUserSchema = z.object({
+  name: z
+    .string({
+      message: "Name is required",
+    })
+    .trim()
+    .min(2, {
+      message: "Name is required",
+    }),
+  email: z
+    .string({
+      required_error: "E-mail is required",
+    })
+    .email({
+      message: "Please provide a vaid e-mail",
+    })
+    .trim()
+    .min(1, {
+      message: "E-mail is required",
+    }),
+  password: z
+    .string({
+      message: "Password is required",
+    })
+    .trim()
+    .min(6, {
+      message: "Password must have at least 6 characters",
+    }),
+})
