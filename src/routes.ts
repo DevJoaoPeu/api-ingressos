fa4b580f844ec1c7d9910a97addc0006e6da2986
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-17 22:21:45 -0300

    feat: adding route createdUser

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from "express"
+import { makeCreateUserController } from "@/factories/user/user"
 
 const router = Router()
 
-router.get("/", (request: Request, response: Response) => {
-  response.send("hellodsdvvsc")
+router.post("/user/register", async (request: Request, response: Response) => {
+  const createUserController = makeCreateUserController()
+  const { statusCode, body } = await createUserController.execute(request)
+
+  response.status(statusCode).send(body)
 })
 
 export { router }
