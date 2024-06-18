a81604935206d45bf1701fe6f18d23f4fc8e8aaf
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-17 22:18:27 -0300

    feat: adding Zod and EmailAlreadyExists error instance checks

--- src/user/createUser/create-user-controller.ts
+++ src/user/createUser/create-user-controller.ts
@@ -1,5 +1,9 @@
+import { badRequest, ok, serverError } from "@/erros/helpers/http"
+import { EmailAlreadyExists } from "@/erros/user/EmailAlreadyExists"
+import { createUserSchema } from "@/schemas/user/user"
 import { IHttpRequest } from "@/types/user/type"
 import { CreateUserUseCase } from "@/user/createUser/create-user-use-case"
+import { ZodError } from "zod"
 
 export class CreateUserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {
@@ -9,11 +13,22 @@ export class CreateUserController {
     try {
       const params = httpRequest.body
 
+      await createUserSchema.parseAsync(params)
+
       const createdUser = await this.createUserUseCase.execute(params)
 
-      return createdUser
+      return ok(createdUser)
     } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest({
+          message: error.errors[0].message,
+        })
+      }
+      if (error instanceof EmailAlreadyExists) {
+        return badRequest({ message: error.message })
+      }
       console.log(error)
+      return serverError()
     }
   }
 }
