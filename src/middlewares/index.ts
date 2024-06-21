b4703ca1c0a557e1f48f423e1aa5be612d5a11a0
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-06-21 14:57:52 -0300

    feat: creating middleware to ensure that the user is logged in when making a request

---
+++ src/middlewares/index.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express"
+import { verify } from "jsonwebtoken"
+
+interface Payload {
+    sub: string
+}
+
+export const isAuthenticated = (
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    const authToken = request.headers.authorization
+
+    if(!authToken){
+        return response.status(401).end()
+    }
+
+    const [,token] = authToken.split(" ")
+
+    try {
+        const {sub} = verify(token, process.env.JWT_SECRET as string) as Payload
+
+        request.user_id = sub
+
+        return next()
+    } catch (error) {
+        return response.status(401).end()
+    }
+}
