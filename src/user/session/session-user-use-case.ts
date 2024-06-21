ed6d1a6a5eda47a7d1073b99efd8c0074d63ba88
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-06-21 12:57:31 -0300

    refactor: refactoring model sessionUserUseCase

--- src/user/session/session-user-use-case.ts
+++ src/user/session/session-user-use-case.ts
@@ -1,45 +1,36 @@
-import { ok } from "@/erros/helpers/http"
-import { FindUserByEmailRepository } from "../findUserByEmail/find-user-by-email-repository"
-import { userNotFoundResponse } from "@/erros/helpers/validation"
-import { ISessionUserParams } from "@/types/user/type"
-import { compare } from "bcrypt"
-import {sign} from 'jsonwebtoken'
+import { EmailOrPasswordIncorrect } from "@/erros/user/errors";
+import { FindUserByEmailRepository } from "../findUserByEmail/find-user-by-email-repository";
+import { userNotFoundResponse } from "@/erros/helpers/validation";
+import { ISessionUserParams } from "@/types/user/type";
+import { compare } from "bcrypt";
+import { sign } from 'jsonwebtoken';
 
 export class SessionUserUseCase {
   constructor(
     private readonly findUserByEmailRepository: FindUserByEmailRepository
   ) {
-    this.findUserByEmailRepository = findUserByEmailRepository
+    this.findUserByEmailRepository = findUserByEmailRepository;
   }
 
   async execute(sessionUserParams: ISessionUserParams) {
-    const isValidUser = await this.findUserByEmailRepository.execute(
-      sessionUserParams.email
-    )
+    const isValidUser = await this.findUserByEmailRepository.execute(sessionUserParams.email);
 
-    const passMatch = await compare(sessionUserParams.password, isValidUser?.password as string)
-
-    if (
-      !sessionUserParams.email === !isValidUser?.email &&
-      !sessionUserParams.password === !passMatch
-    ) {
-      return userNotFoundResponse()
+    if (!isValidUser || !(await compare(sessionUserParams.password, isValidUser.password))) {
+      throw new EmailOrPasswordIncorrect(sessionUserParams.email)
     }
 
-    const token = sign(
-    {
-      name: isValidUser.name,
-      email: isValidUser.email
-    } ,
-    process.env.JWT_SECRET as string,
-    { subject: isValidUser?.id, expiresIn: '30d'}
-  )
+    const token = sign({
+        email: sessionUserParams.email
+      },
+      process.env.JWT_SECRET as string,
+      { subject: isValidUser.id, expiresIn: '30d' }
+    );
 
     const user = {
       ...isValidUser,
       token
-    }
+    };
 
-    return user
+    return user;
   }
 }
