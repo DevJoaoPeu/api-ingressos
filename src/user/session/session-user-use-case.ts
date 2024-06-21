7fbb9bb13cacb3fd9554bce15a3b46f4262b28dd
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-06-21 11:48:27 -0300

    feat: add SessionUserUseCase

--- src/user/session/session-user-use-case.ts
+++ src/user/session/session-user-use-case.ts
@@ -1,6 +1,9 @@
+import { ok } from "@/erros/helpers/http"
 import { FindUserByEmailRepository } from "../findUserByEmail/find-user-by-email-repository"
 import { userNotFoundResponse } from "@/erros/helpers/validation"
 import { ISessionUserParams } from "@/types/user/type"
+import { compare } from "bcrypt"
+import {sign} from 'jsonwebtoken'
 
 export class SessionUserUseCase {
   constructor(
@@ -8,20 +11,35 @@ export class SessionUserUseCase {
   ) {
     this.findUserByEmailRepository = findUserByEmailRepository
   }
+
   async execute(sessionUserParams: ISessionUserParams) {
     const isValidUser = await this.findUserByEmailRepository.execute(
       sessionUserParams.email
     )
 
+    const passMatch = await compare(sessionUserParams.password, isValidUser?.password as string)
+
     if (
       !sessionUserParams.email === !isValidUser?.email &&
-      !sessionUserParams.password === !isValidUser?.password
+      !sessionUserParams.password === !passMatch
     ) {
       return userNotFoundResponse()
     }
 
-    // const user = {
-    //   ...isValidUser,
-    // }
+    const token = sign(
+    {
+      name: isValidUser.name,
+      email: isValidUser.email
+    } ,
+    process.env.JWT_SECRET as string,
+    { subject: isValidUser?.id, expiresIn: '30d'}
+  )
+
+    const user = {
+      ...isValidUser,
+      token
+    }
+
+    return ok(user)
   }
 }
