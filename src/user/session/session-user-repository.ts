5c78d05bcfdf14a8aa52106827cc0d0d579edb6b
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-20 22:41:49 -0300

    refactor: deleted code duplicate

--- src/user/session/session-user-repository.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from "@/prisma/PrismaClient/prisma"
-
-export class SessionUserRepository {
-  async execute(email: string) {
-    return await prisma.user.findFirst({ where: { email } })
-  }
-}
