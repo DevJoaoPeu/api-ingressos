1a177c6cb99cc7008e920dba9839e8f080c3e1ab
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-23 19:01:37 -0300

    feat: creating error for invalid date and id not found

--- src/event/createEvent/create-event-controller.ts
+++ src/event/createEvent/create-event-controller.ts
@@ -3,6 +3,7 @@ import { IEventHttpRequest } from "../types"
 import { CreateEventUseCase } from "./create-event-use-case"
 import { badRequest, ok, serverError } from "@/erros/helpers/http"
 import { ZodError } from "zod"
+import { DateInvalid, IdNotFound } from "@/erros/event/errors"
 
 export class CreateEventController {
   constructor(private readonly createEventUseCase: CreateEventUseCase) {
@@ -22,6 +23,18 @@ export class CreateEventController {
       if (error instanceof ZodError) {
         return badRequest({
           message: error.errors[0].message,
+        })
+      }
+
+      if (error instanceof IdNotFound) {
+        return badRequest({
+          message: error.message,
+        })
+      }
+
+      if (error instanceof DateInvalid) {
+        return badRequest({
+          message: error.message,
         })
       }
       console.error(error)
