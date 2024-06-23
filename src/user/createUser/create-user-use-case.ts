86a300b7f7f31ec43cab12a237f6c3bd441b07e6
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-23 17:53:24 -0300

    fix: correcting type path

--- src/user/createUser/create-user-use-case.ts
+++ src/user/createUser/create-user-use-case.ts
@@ -1,4 +1,4 @@
-import { ICreateUserParams } from "@/types/user/type"
+import { ICreateUserParams } from "@/user/type"
 import { CreateUserRepository } from "./create-user-repository"
 import { FindUserByEmailRepository } from "@/user/findUserByEmail/find-user-by-email-repository"
 import { EmailAlreadyExists } from "@/erros/user/errors"
