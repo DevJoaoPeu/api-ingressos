22798a3a557b04c76b5ea97e69fd5024c4f8a8ce
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-06-24 14:54:39 -0300

    feat: add FindAllEventsByUserIdRepository

---
+++ src/event/findAllEventsByUserId/find-all-events-repository.ts
@@ -0,0 +1,11 @@
+import { prisma } from "@/prisma/PrismaClient/prisma"
+
+export class FindAllEventsByUserIdRepository {
+  async execute(userId: string) {
+    return await prisma.event.findMany({
+      where: {
+        userId,
+      },
+    })
+  }
+}
