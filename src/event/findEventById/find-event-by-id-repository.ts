bb0b52cf2c4687b1a1c3d0600987fd659c200b47
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-06-24 22:54:45 -0300

    feat: add FindEventByIdRepository

---
+++ src/event/findEventById/find-event-by-id-repository.ts
@@ -0,0 +1,11 @@
+import { prisma } from "@/prisma/PrismaClient/prisma"
+
+export class FindEventByIdRepository {
+  async execute(eventId: string) {
+    return await prisma.event.findUnique({
+      where: {
+        id: eventId,
+      },
+    })
+  }
+}
