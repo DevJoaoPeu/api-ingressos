9112500159e3c26d7fe005070ed7a5342c2f4fc4
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-06-25 12:55:39 -0300

    feat: add UpdateEventController

---
+++ src/event/updateEvent/update-event-controller.ts
@@ -0,0 +1,36 @@
+import { badRequest, ok, serverError } from "@/erros/helpers/http"
+import { IUpdateEventParams } from "../types"
+import { UpdateEventUseCase } from "./update-event-use-case"
+import { updateEventSchema } from "@/schemas/event/event"
+import { eventNotFoundResponse } from "@/erros/helpers/validation"
+import { ZodError } from "zod"
+
+export class UpdateEventController {
+  constructor(private readonly updateEventUseCase: UpdateEventUseCase) {
+    this.updateEventUseCase = updateEventUseCase
+  }
+  async execute(httpRequest: IUpdateEventParams) {
+    try {
+      const eventId = httpRequest.params.eventId
+      const params = httpRequest.body
+
+      await updateEventSchema.parseAsync(params)
+
+      const event = await this.updateEventUseCase.execute(eventId, params)
+
+      if (!event) {
+        return eventNotFoundResponse()
+      }
+
+      return ok(event)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest({
+          message: error.errors[0].message,
+        })
+      }
+      console.error(error)
+      return serverError()
+    }
+  }
+}
