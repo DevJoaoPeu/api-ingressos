0a9025b2959a608819b53c0ea1fa32c2f9387ca4
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-07-03 17:22:32 -0300

    feat: typing parameters, correcting them at home

--- src/ticket/findTicketByType/find-ticket-by-type-repository.ts
+++ src/ticket/findTicketByType/find-ticket-by-type-repository.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/prisma/PrismaClient/prisma"
 import { TicketType } from "@prisma/client"
 
-export class FindTicketByType {
+export class FindTicketByTypeRepository {
   async execute(type: TicketType) {
     return await prisma.ticket.findMany({
       where: { type },
--- src/ticket/findTicketByType/find-ticket-by-type-use-case.ts
+++ src/ticket/findTicketByType/find-ticket-by-type-use-case.ts
@@ -1,6 +1,5 @@
 import { IFindAllTicketId } from "../type"
 import { FindTicketByTypeRepository } from "./find-ticket-by-type-repository"
-
 export class FindTicketByTypeUseCase {
   constructor(
     private readonly findTicketByTypeRepository: FindTicketByTypeRepository
