2e6af51d662053463d80402c7666e92d25519171
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-07-02 21:10:00 -0300

    feat: adding factory findTicket

--- src/factories/ticket/ticket.ts
+++ src/factories/ticket/ticket.ts
@@ -2,6 +2,9 @@ import { FindEventByIdRepository } from "@/event/findEventById/find-event-by-id-
 import { CreateTicketController } from "@/ticket/createTicket/create-ticket-controller"
 import { CreateTicketRepository } from "@/ticket/createTicket/create-ticket-repository"
 import { CreateTicketUseCase } from "@/ticket/createTicket/create-ticket-use-case"
+import { FindTicketByIdController } from "@/ticket/findTicketById/find-ticket-by-id-controller"
+import { FindTicketByIdRepository } from "@/ticket/findTicketById/find-ticket-by-id-repository"
+import { FindTicketByIdUseCase } from "@/ticket/findTicketById/find-ticket-by-id-use-case"
 
 export const makeCreateTicketController = () => {
   const createTicketRepository = new CreateTicketRepository()
@@ -15,4 +18,17 @@ export const makeCreateTicketController = () => {
   const createTicketController = new CreateTicketController(createTicketUseCase)
 
   return createTicketController
+}
+
+export const makeFindTicketByIdController = () => {
+  const findTicketByIdRepository = new FindTicketByIdRepository()
+  const findTicketByIdUseCase = new FindTicketByIdUseCase(
+    findTicketByIdRepository
+  )
+
+  const findTicketByIdController = new FindTicketByIdController(
+    findTicketByIdUseCase
+  )
+
+  return findTicketByIdController
 }
