ce6258e71c05a28f9084ac87c88f68e4c769f3f2
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-07-02 22:22:21 -0300

    feat: creating in the bank the number of tickets passed by the user

--- src/ticket/createTicket/create-ticket-repository.ts
+++ src/ticket/createTicket/create-ticket-repository.ts
@@ -3,6 +3,11 @@ import { ICreateTicketParams } from "../type"
 
 export class CreateTicketRepository {
   async execute(createTicketParams: ICreateTicketParams) {
-    return await prisma.ticket.create({ data: createTicketParams })
+    const inserts = []
+    for (let i = 0; i < createTicketParams.qtTicket; i++) {
+      const newTicket = await prisma.ticket.create({ data: createTicketParams })
+      inserts.push(newTicket)
+    }
+    return inserts
   }
 }
