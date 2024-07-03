f1f4c8afcbc581c2065005b683c71937295445cf
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-07-03 16:39:00 -0300

    feat: add factory to makeFindAllTicketsByEventIdController

--- src/factories/ticket/ticket.ts
+++ src/factories/ticket/ticket.ts
@@ -2,6 +2,9 @@ import { FindEventByIdRepository } from "@/event/findEventById/find-event-by-id-
 import { CreateTicketController } from "@/ticket/createTicket/create-ticket-controller"
 import { CreateTicketRepository } from "@/ticket/createTicket/create-ticket-repository"
 import { CreateTicketUseCase } from "@/ticket/createTicket/create-ticket-use-case"
+import { FindAllTicketsByEventIdController } from "@/ticket/findAllTicketsByEventId/find-all-tickets-by-eventId-controller"
+import { FindAllTicketsByEventIdRepository } from "@/ticket/findAllTicketsByEventId/find-all-tickets-by-eventId-repository"
+import { FindAllTicketsByEventIdUseCase } from "@/ticket/findAllTicketsByEventId/find-all-tickets-by-eventId-use-case"
 import { FindTicketByIdController } from "@/ticket/findTicketById/find-ticket-by-id-controller"
 import { FindTicketByIdRepository } from "@/ticket/findTicketById/find-ticket-by-id-repository"
 import { FindTicketByIdUseCase } from "@/ticket/findTicketById/find-ticket-by-id-use-case"
@@ -31,4 +34,17 @@ export const makeFindTicketByIdController = () => {
   )
 
   return findTicketByIdController
+}
+
+export const makeFindAllTicketsByEventIdController = () => {
+  const findAllTicketsByEventIdRepository =
+    new FindAllTicketsByEventIdRepository()
+  const findAllTicketsByEventIdUseCase = new FindAllTicketsByEventIdUseCase(
+    findAllTicketsByEventIdRepository
+  )
+
+  const findAllTicketsByEventIdController =
+    new FindAllTicketsByEventIdController(findAllTicketsByEventIdUseCase)
+
+  return findAllTicketsByEventIdController
 }
