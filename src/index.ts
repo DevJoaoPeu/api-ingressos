9625d41c33256d320f9f90d10aa1b510c90f3b57
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-07-06 17:19:52 -0300

    chore: changes to the use of swagger

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import * as Express from "express"
+import express from "express"
 
 import * as dotenv from "dotenv"
 import { router } from "@/routes"
+import { serve, setup } from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
-const app = Express()
+const app = express()
 
-app.use(Express.json())
+app.use(express.json())
+
+app.use("/docs", serve, setup(swaggerDocs))
 app.use(router)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
