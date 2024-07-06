e7ce855752bbcfc913a9405c055acbd35ef4ee9b
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-07-06 19:53:59 -0300

    feat: add CreateSaleRepository

---
+++ src/sale/createSales/create-sale-repository.ts
@@ -0,0 +1,10 @@
+import { prisma } from "@/prisma/PrismaClient/prisma"
+import { Sale } from "@prisma/client"
+
+export class CreateSaleRepository {
+  async execute(createSaleParams: Sale) {
+    return await prisma.sale.create({
+      data: createSaleParams,
+    })
+  }
+}
