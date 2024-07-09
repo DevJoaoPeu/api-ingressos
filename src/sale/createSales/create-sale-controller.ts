7add6e35d83ed2080ee21e75f44ac8f3726b7825
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-07-09 20:22:14 -0300

    feat: add CreateSaleController

---
+++ src/sale/createSales/create-sale-controller.ts
@@ -0,0 +1,39 @@
+import { badRequest, ok, serverError } from "@/erros/http"
+import { CreateSaleUseCase } from "./create-sale-use-case"
+import {
+  QtTicketUnavailable,
+  TicketNotFound,
+  UserNotFound,
+} from "@/erros/errors"
+import { IHttpParamsSale } from "../type"
+
+export class CreateSaleController {
+  constructor(private readonly createSaleUseCase: CreateSaleUseCase) {}
+  async execute(httpParams: IHttpParamsSale) {
+    try {
+      const params = httpParams.body
+
+      const createSale = await this.createSaleUseCase.execute(params)
+
+      return ok(createSale)
+    } catch (error) {
+      if (error instanceof UserNotFound) {
+        return badRequest({
+          message: error.message,
+        })
+      }
+      if (error instanceof TicketNotFound) {
+        return badRequest({
+          message: error.message,
+        })
+      }
+      if (error instanceof QtTicketUnavailable) {
+        return badRequest({
+          message: error.message,
+        })
+      }
+      console.error(error)
+      return serverError()
+    }
+  }
+}
---
+++ src/sale/type.ts
@@ -0,0 +1,5 @@
+import { Sale } from "@prisma/client"
+
+export interface IHttpParamsSale {
+  body: Sale
+}
