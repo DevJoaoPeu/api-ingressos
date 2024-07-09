35fa9e04abce664581f23e4c93f67d3397c113f8
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-07-08 22:02:11 -0300

    fix: accessing wrong variable

--- src/controlleTickets/createControlleTicket/create-controlleTicket-use-case.ts
+++ src/controlleTickets/createControlleTicket/create-controlleTicket-use-case.ts
@@ -1,12 +1,14 @@
 import { ControllerTicket } from "@prisma/client"
 import { CreateControlleTicketRepository } from "./create-controlleTickets-repository"
 import { FindEventByIdRepository } from "@/event/findEventById/find-event-by-id-repository"
-import { EventNotFound } from "@/erros/errors"
+import { ControlleTicketExists, EventNotFound } from "@/erros/errors"
+import { FindControlleTicketByTypeRepository } from "../findControlleTicketByType/find-controlle-ticket-by-type-repository"
 
 export class CreateControlleTicketUseCase {
   constructor(
     private readonly createControlleTicketRepository: CreateControlleTicketRepository,
-    private readonly findEventByIdRepository: FindEventByIdRepository
+    private readonly findEventByIdRepository: FindEventByIdRepository,
+    private readonly findControlleTicketByTypeRepository: FindControlleTicketByTypeRepository
   ) {}
   async execute(createControlleTicketParams: ControllerTicket) {
     const eventExists = await this.findEventByIdRepository.execute(
@@ -15,6 +17,15 @@ export class CreateControlleTicketUseCase {
 
     if (!eventExists) {
       throw new EventNotFound()
+    }
+
+    const findControlleTicket =
+      await this.findControlleTicketByTypeRepository.execute(
+        createControlleTicketParams.type
+      )
+
+    if (findControlleTicket) {
+      throw new ControlleTicketExists(createControlleTicketParams.type)
     }
 
     const createControlleTicket =
