46d913ef4ac1aa8ee994a0db535f9566faab9591
DevJoaoPeu <joaopedroprog.contato@gmail.com>
2024-07-15 12:22:36 -0300

    fix: error type

--- src/controlleTickets/findControlleTicketByType/find-controlle-ticket-by-type-repository.ts
+++ src/controlleTickets/findControlleTicketByType/find-controlle-ticket-by-type-repository.ts
@@ -1,7 +1,8 @@
 import { prisma } from "@/prisma/PrismaClient/prisma"
+import { TicketType } from "@prisma/client"
 
 export class FindControlleTicketByTypeRepository {
-  async execute(type: string) {
+  async execute(type: TicketType) {
     return await prisma.controllerTicket.findFirst({
       where: {
         type,
