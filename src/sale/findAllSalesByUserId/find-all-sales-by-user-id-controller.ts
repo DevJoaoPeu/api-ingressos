ed3d5d3aec36d6ee166c846c44c677b6d09e7170
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-07-16 21:09:18 -0300

    fix: returning message from zod if there is an error

--- src/sale/findAllSalesByUserId/find-all-sales-by-user-id-controller.ts
+++ src/sale/findAllSalesByUserId/find-all-sales-by-user-id-controller.ts
@@ -1,7 +1,8 @@
-import { ok, serverError } from "src/erros/http"
+import { badRequest, ok, serverError } from "src/erros/http"
 import { FindAllSalesByUserIdUseCase } from "./find-all-sales-by-user-id-use-case"
 import { isValidIdSchema } from "src/schemas/user"
 import { IUserByIdParams } from "src/user/type"
+import { ZodError } from "zod"
 
 export class FindAllSalesByUserIdController {
   constructor(
@@ -17,6 +18,11 @@ export class FindAllSalesByUserIdController {
 
       return ok(sales)
     } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest({
+          message: error.errors[0].message,
+        })
+      }
       console.error(error)
       return serverError()
     }
