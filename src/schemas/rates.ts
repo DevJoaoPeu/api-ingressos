3aced95eef8cf192710f78fe11295dcbf4182fc8
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-09-01 16:02:29 -0300

    feat: creating schema for fee table

---
+++ src/schemas/rates.ts
@@ -0,0 +1,28 @@
+import { z } from "zod"
+
+export const createRatesSchema = z.object({
+  description: z
+    .string({
+      message: "Description is required",
+    })
+    .trim()
+    .min(5, {
+      message: "Description is required",
+    })
+    .max(200, {
+      message: "Very large description",
+    })
+    .optional(),
+  userId: z
+    .string({
+      message: "userId is required",
+    })
+    .uuid({
+      message: "invalid uuid",
+    })
+    .trim()
+    .min(1, {
+      message: "userId is required",
+    }),
+  rate: z.number({ message: "Rate is required" }),
+})
